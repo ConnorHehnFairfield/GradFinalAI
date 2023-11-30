5c64f578eaf676edc165dd66d67db4c83e6091bc
ConnorHehnFairfield <117684841+ConnorHehnFairfield@users.noreply.github.com>
2023-11-30 15:49:44 -0500

    Add files via upload

---
+++ vid-to-image.py
@@ -0,0 +1,19 @@
+import cv2
+
+video_path = 'videos/IMG_0054.mp4'
+output_folder = 'frames/'
+
+cap = cv2.VideoCapture(video_path)
+frame_count = 0
+
+while cap.isOpened():
+    ret, frame = cap.read()
+
+    if not ret:
+        break
+
+    frame_count += 1
+    frame_filename = f"{output_folder}frame_{frame_count:04d}.jpg"
+    cv2.imwrite(frame_filename, frame)
+
+cap.release()
